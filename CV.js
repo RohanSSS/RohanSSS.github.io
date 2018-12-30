2bf70db778ce7713fd2717364404eff1ee110867
RohanSSS <rohan_shah_summers@yahoo.co.uk>
2018-12-30 18:40:34 +0000

    Update CV.js

--- CV.js
+++ CV.js
@@ -1,5 +1,8 @@
 let p1 = "<p> \
-  place cv text here  \
+I am a young person keen to become a part of the software development industry. \
+I have been an avid player of video games for many years, and the hidden side behind the game is one of the largest drivers behind my interest in the world of software development. \
+Puzzles are something that I have always enjoyed doing, and my enthusiasm with these is something that carries over as each program needing development is a puzzle to be solved.  \
+<br /><br />  \
   <\/p>";
 
 document.write(p1);
