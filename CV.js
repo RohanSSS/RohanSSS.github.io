44845a77d53e9680b4083486af1e584f697a0735
RohanSSS <rohan_shah_summers@yahoo.co.uk>
2018-12-31 01:58:37 +0000

    cv
    added why fac interests me

--- CV.js
+++ CV.js
@@ -2,7 +2,12 @@ let p1 = "<p> \
 I am a young person keen to become a part of the software development industry. \
 I have been an avid player of video games for many years, and the hidden side behind the game is one of the largest drivers behind my interest in the world of software development. \
 Puzzles are something that I have always enjoyed doing, and my enthusiasm with these is something that carries over as each program needing development is a puzzle to be solved.  \
-<br /><br />  \
+<br /><br /> Founders and Coders interests me due to it being a gatway into the world of web development, and software development beyond that. I am interested in participating in a peer led course because I enjoy working with other people. I have worked closely with peers and in a team in order to achieve goals. This has left me with a great appreciation of pair programming as a way of learning, as you work together to expand eachothers knowledge.Therefore a course that is concentrated on peer and team based learning is something that interests me much more than simply learning on my own. Working with a peer or team is a much better environment to prepare for the wider world of work, and is a skill that I have begun developing already, but I believe would be greatly enhanced by the course. \
+<br /><br /> \
+My interest in programming is one that was initially developed from a curiosity into how the video games I played worked. This developed into a broad interest of technology, both software and hardware, and the workings behind it. I started learning programming languages during high school. \
+Web development as a career interests me as there are so many different types of websites on the internet. The thought of expanding what can be done with programming languages, and what it means to create a website is something that I find incredible and I would love to learn and advance into a position where I am contributing to this ever growing area, and become a future contributor. \
+<br /><br /> \
+Being part of something where you will be helping the future applicants become a part of the industry is also something that intrigues me. I want to be a part of a community that strives for and supports new people to become a part of it, bringing new and innovative ideas to the industry. I believe this keeps people connected and develops ideas, allowing for more innovation. \
   <\/p>";
 
 document.write(p1);
--- prerequisites.js
+++ prerequisites.js
@@ -1,5 +1,12 @@
-let p2 = "<p> \
-  place prerequistes text here  \
+let p2 = "<p class='tabPara'> \
+  Much of the things that I encountered while doing the prerequisites for the application were things that I had encountered before in my own studies of programming languages.  \
+  However tha subject that was most enlightening for me was ES6 within javascript on freeCodeCamp. \
+  ES6 was something that I had encountered before in passing, but never actually explored. \
+  As such the module on it within freeCodeCamp was my first usage of it to an extent. \
+  I now have a much greater understanding of what ES6 is and whats its function is for use with javascript. \
+  I have used ES6 with coding before, but this was always small sections that were for the specific thing that I was looking for. \
+  I hadn't taken a deep dive into what exactly it was fully capable of. \
+  The prerequisites meant that I took a deeper look into what exactly ES6 was as an extension of javascript, and what it could be used for. \
   <\/p>";
 
 document.write(p2);
